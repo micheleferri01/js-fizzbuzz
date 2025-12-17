3d402175237010c0b5636f579066d7eae29b23c5
micheleferri01 <micheleferri095@gmail.com>
2025-12-17 16:49:10 +0100

    bonus

--- js/main.js
+++ js/main.js
@@ -17,31 +17,46 @@ const arriveTo = 100;
 const multipleNum1 = 3;
 // Dichiaro il secondo multiplo del quale desidero conoscere i numeri
 const multipleNum2 = 5;
+const multipleOfBoth = multipleNum1 * multipleNum2;
+const multipleNum1Message = "Fizz";
+const multipleNum2Message = "Buzz";
 
-let message;
+// let message;
 
 // ##Elaborazione
 // Creo ciclo per iniziare a contare
-for (let i = startFrom; i <= arriveTo; i++){
+/*for (let i = startFrom; i <= arriveTo; i++){
     // SE (il numero è multiplo di 3 AND multiplo di 5)
-    if((i % multipleNum1 === 0) && (i % multipleNum2 === 0)){
+    if(i % multipleOfBoth === 0){
         // Scrivo nella console FizzBuzz al posto del numero
-        message = console.log("FizzBuzz");
+        message = console.log(multipleNum1Message+multipleNum2Message);
     }
     // ALTRIMENTI SE (il numero è multiplo di 3)
     else if (i % multipleNum1 === 0){
         // Scrivo nella console Fizz al posto del numero
-        message = console.log("Fizz");
+        message = console.log(multipleNum1Message);
     }
     // ALTRIMENTI SE (il numero è multiplo di 5)
     else if (i % multipleNum2 === 0){
         // Scrivo nella console Buzz al posto del numero
-        message = console.log("Buzz");
+        message = console.log(multipleNum2Message);
     }
     else {
         message = console.log(i);
     }
-}
+}*/
 
 // ##Output
 // Scrivo la lista dei numeri con le sostituzioni richieste
+
+// #BONUS
+for (let i = startFrom; i <= arriveTo; i++){
+    let message = "";
+    if(i % multipleNum1 === 0){
+        message += multipleNum1Message;
+    }
+    if(i % multipleNum2 === 0){
+        message += multipleNum2Message;
+    }
+    console.log(message || i);
+}
