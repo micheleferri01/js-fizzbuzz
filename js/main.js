89fa005f284cda8cc86e663a7cffce2416c52f4b
micheleferri01 <micheleferri095@gmail.com>
2025-12-17 14:42:51 +0100

    Pseudo codice

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// *js-fizzbuzz
+// Richiesta
+/*
+ Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+// #Pseudo codice
+
+// ##Raccolta dati
+// Dichiaro da che numero desidero iniziare a contare
+// Dichiaro fino a che numero desidero contare
+// Dichiaro il primo multiplo del quale desidero conoscere i numeri
+// Dichiaro il secondo multiplo del quale desidero conoscere i numeri
+
+// ##Elaborazione
+// Creo ciclo per iniziare a contare
+    // SE (il numero è multiplo di 3 AND multiplo di 5)
+        // Scrivo nella console FizzBuzz al posto del numero
+    // ALTRIMENTI SE (il numero è multiplo di 3)
+        // Scrivo nella console Fizz al posto del numero
+    // ALTRIMENTI SE (il numero è multiplo di 5)
+        // Scrivo nella console Buzz al posto del numero
+
+// ##Output
+// Scrivo la lista dei numeri con le sostituzioni richieste
